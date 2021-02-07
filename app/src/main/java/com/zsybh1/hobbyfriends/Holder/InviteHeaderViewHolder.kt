33c5b37ec906f7a03b5bbb4619422c223c6588dc
zsybh1 <zsybh1@163.com>
2021-02-07 09:10:57 +0800

    点赞显示

--- app/src/main/java/com/zsybh1/hobbyfriends/Holder/InviteHeaderViewHolder.kt
+++ app/src/main/java/com/zsybh1/hobbyfriends/Holder/InviteHeaderViewHolder.kt
@@ -1,10 +1,12 @@
 package com.zsybh1.hobbyfriends.Holder
 
+import android.content.Context
 import android.content.Intent
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.zsybh1.hobbyfriends.Model.Invitation
+import com.zsybh1.hobbyfriends.R
 import com.zsybh1.hobbyfriends.Topic.TopicActivity
 import com.zsybh1.hobbyfriends.Utils.BitmapUtil
 import com.zsybh1.hobbyfriends.Utils.TimeUtil
@@ -16,12 +18,21 @@ class InviteHeaderViewHolder(private val view: View) : RecyclerView.ViewHolder(v
     fun bind(data: Invitation, fragment: Fragment){
         view.tvTitle.visibility = View.GONE
         view.imPicture.visibility = View.GONE
+        view.imSub.setImageResource(R.drawable.ic_outline_thumb_up_24)
 
         view.tvUsername.text =  data.ownerName
         view.tvTime.text = TimeUtil.getRelativeTimeString(TimeUtil.getLDTfromString(data.sendDate))
         view.tvComment.text = if (data.comments != null) data.comments.size.toString() else "0"
         view.tvContent.text = data.context
         view.tvSub.text = (data.likes?:0).toString()
+        val userid = fragment.requireActivity().getSharedPreferences("save", Context.MODE_PRIVATE).getLong("userid", 0L)
+        if (data.likeList != null) {
+            for (user in data.likeList) {
+                if (userid == user.id) {
+                    view.imSub.setImageResource(R.drawable.ic_baseline_thumb_up_24)
+                }
+            }
+        }
 
         if (data.title != "") {
             view.tvTitle.visibility = View.VISIBLE
--- app/src/main/java/com/zsybh1/hobbyfriends/Holder/TopicHeaderViewHolder.kt
+++ app/src/main/java/com/zsybh1/hobbyfriends/Holder/TopicHeaderViewHolder.kt
@@ -1,10 +1,12 @@
 package com.zsybh1.hobbyfriends.Holder
 
+import android.content.Context
 import android.content.Intent
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.zsybh1.hobbyfriends.Model.Topic
+import com.zsybh1.hobbyfriends.R
 import com.zsybh1.hobbyfriends.Topic.TopicActivity
 import com.zsybh1.hobbyfriends.Utils.BitmapUtil
 import com.zsybh1.hobbyfriends.Utils.TimeUtil
@@ -14,12 +16,22 @@ class TopicHeaderViewHolder(private val view : View) : RecyclerView.ViewHolder(v
     fun bind(data:Topic, fragment: Fragment){
         view.tvTitle.visibility = View.GONE
         view.imPicture.visibility = View.GONE
+        view.imSub.setImageResource(R.drawable.ic_outline_thumb_up_24)
 
         view.tvUsername.text =  data.username
         view.tvTime.text = TimeUtil.getRelativeTimeString(TimeUtil.getLDTfromString(data.sendDate))
         view.tvComment.text = if (data.comments !=null) (data.comments.size).toString() else "0"
         view.tvContent.text = data.context
         view.tvSub.text = (data.likes?:0).toString()
+
+        val userid = fragment.requireActivity().getSharedPreferences("save", Context.MODE_PRIVATE).getLong("userid", 0L)
+        if (data.likeList != null) {
+            for (user in data.likeList) {
+                if (userid == user.id) {
+                    view.imSub.setImageResource(R.drawable.ic_baseline_thumb_up_24)
+                }
+            }
+        }
 
         if (data.title != "") {
             view.tvTitle.visibility = View.VISIBLE
